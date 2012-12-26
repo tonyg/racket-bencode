b9e72e9ab433cda25d4e7423a5acfb2cf8bbe735
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2012-12-26 18:10:17 +1300

    info.rkt

---
+++ info.rkt
@@ -0,0 +1,11 @@
+#lang setup/infotab
+(define name "bencode")
+(define blurb
+  (list
+   `(p "Reading and writing of Bencoded data")))
+(define categories '(net))
+(define can-be-loaded-with 'all)
+(define homepage "https://github.com/tonyg/racket-bencode")
+(define primary-file "main.rkt")
+(define repositories '("4.x"))
+(define scribblings '(("manual.scrbl" ())))
