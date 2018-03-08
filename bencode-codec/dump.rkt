cf4161c67e0a6f3f25fa162b9f61a3460b4ce445
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2018-03-08 18:17:10 +0000

    Larger limit for bencode-dump

--- bencode-codec/dump.rkt
+++ bencode-codec/dump.rkt
@@ -4,6 +4,8 @@
   (require "main.rkt")
   (require racket/pretty)
 
+  (bencode-bytes-limit 1048576)
+
   (let loop ()
     (define term (bencode-read (current-input-port)))
     (when (not (eof-object? term))
